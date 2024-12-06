7d708d4420cffb15292b34adb6ca29a54e51af3a
rahul4019 <rahulkashyap4019@gmail.com>
2024-12-06 19:09:38 +0530

    Cpython and Jython

---
+++ mcs-201/python/07_cpython.py
@@ -0,0 +1,16 @@
+# C-PYTHON
+# c-python refers to the default implementation of python programming language, written in the
+# c programming language. it is the most widely used implementation of python and serves as the
+# reference implementation of the language.
+
+# key features of CPython
+# 1. written in C:
+#     * cpython is implemented in the c language, which allows it to be efficient and portable.
+#     * it compiles python source code into bytecode, which is then executed by the cpython
+#       interpretor
+
+# 2. interpreted language:
+#   cpython executes python code line by line after converting it to bytecode. this means python
+#   programs do not need a separate compilation step like c and java.
+
+# cpython is the interpretor of the python language
---
+++ mcs-201/python/08_jython.py
@@ -0,0 +1,19 @@
+# Jython is an implementation of the Python programming language that runs on the Java Virtual Machine
+# (JVM). It allows you to write Python code that can seamlessly interact with Java classes and libraries.
+
+# Key Features of Jython:
+# Python on the JVM: Jython is a Python interpreter written in Java that compiles Python code into Java bytecode,
+# allowing Python to run on the JVM.
+#
+# Interoperability: Jython enables Python code to easily interact with Java libraries, classes, and frameworks.
+# You can import Java packages and use them just like Python modules.
+#
+# No Need for a Separate Interpreter: Unlike standard Python (CPython), which requires its own interpreter,
+# Jython uses the JVM to run Python code, making it easier to integrate Python into Java-based applications.
+#
+# How Jython Works:
+# Compilation to Bytecode: When you run a Python script using Jython, the script is first compiled into Java bytecode,
+# not native Python bytecode. This bytecode is then executed by the JVM.
+#
+# Integration with Java: Jython allows direct interaction with Java code. For example, you can instantiate Java objects,
+# call Java methods, and inherit from Java classes using Python syntax.
