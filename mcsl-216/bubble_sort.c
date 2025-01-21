272be5376ba7dba61025c06c25b0f08771469fd0
rahul4019 <rahulkashyap4019@gmail.com>
2025-01-21 19:46:18 +0530

    Add counter metrics to the bubble sort

--- mcsl-216/bubble_sort.c
+++ mcsl-216/bubble_sort.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 
 void bubble_sort(int arr[], int size) {
+  int swap_counter = 0;
+  int comparison_counter = 0;
+  int inner_loop_counter = 0;
+  int outer_loop_counter = 0;
+
   for (int i = 0; i < size - 1; i++) {
+    outer_loop_counter++;
     for (int j = 0; j < size - i - 1; j++) {
+      inner_loop_counter++;
+      comparison_counter++;
       if (arr[j] > arr[j + 1]) {
         int temp = arr[j];
         arr[j] = arr[j + 1];
         arr[j + 1] = temp;
+        swap_counter++;
       }
     }
   }
+  printf("swaps count: %d\n", swap_counter);
+  printf("comparison count: %d\n", comparison_counter);
+  printf("outer loop count: %d\n", outer_loop_counter);
+  printf("inner loop count : %d\n", inner_loop_counter);
 }
 
 int main() {
